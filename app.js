ce89d45feed9e357dddbc64dd47ec7b374481f52
Mussa Charles <mussacharles60@gmail.com>
2022-01-26 16:36:13 +0300

    added expressjs onboard

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser') // For Express version less than 4.16.0
+var path = require('path');
+
+const port = process.env.PORT || "8000";
+
+var app = express();
+
+app.use(cookieParser());
+app.use(express.json()); // For Express v4.16.0 and higher
+// app.use(bodyParser.urlencoded({ extended: true }));+
+// app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get('/quiz', function(req, res) {
+    res.sendFile(__dirname + "/views/quiz.html");
+});
+
+app.post('/', function(req, res) {
+    console.log("request: ", req.body);
+    res.send("Successfully Submitted. Please continue to next section");
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/test.html");
+});
+
+var server = app.listen(port, function() {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://localhost:" + port)
+})

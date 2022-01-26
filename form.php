482a98245f2dcf8b2c596bddf1bf92d153d9ae1d
Massawe14 <ramadhanimassawe14@gmail.com>
2022-01-26 16:21:22 +0300

    update code

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="./plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="./plugins/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./plugins/DataTables/datatables.min.css">
+  <link rel="stylesheet" href="./plugins/css/custom.css">
+  <script src="./plugins/js/jquery-3.6.0.min.js"></script>
+  <script src="./plugins/js/popper.min.js"></script>
+  <script src="./plugins/js/jquery-ui.js"></script>
+  <script src="./plugins/js/bootstrap.min.js"></script>
+  <script src="./plugins/js/script.js"></script>
+  <title>Create Form</title>
+</head>
+<body>
+</body>
+</html>

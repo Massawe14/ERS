f81ed936f4745d022094d4946736b15daec1425a
Massawe14 <ramadhanimassawe14@gmail.com>
2022-02-07 13:39:16 +0300

    update

--- code.php
+++ code.php
@@ -1,25 +1,27 @@
 <?php 
   include ('plugins/meRaviQr/qrlib.php');
   include ('plugins/config.php');
+  include('config/dbconn.php');
 
   if(isset($_POST['create'])) {
-        
-    $event_id = isset($_POST['event_id']) ? $_POST['event_id'] : '';
-  	$field_1 =  isset($_POST['field_1']) ? $_POST['field_1'] : '';
-    $field_2 =  isset($_POST['field_2']) ? $_POST['field_2'] : '';
-    $field_3 =  isset($_POST['field_3']) ? $_POST['field_3'] : '';
-    $field_4 =  isset($_POST['field_4']) ? $_POST['field_4'] : '';
-    $field_5 =  isset($_POST['field_5']) ? $_POST['field_5'] : '';
-    $field_6 =  isset($_POST['field_6']) ? $_POST['field_6'] : '';
-    $field_7 =  isset($_POST['field_7']) ? $_POST['field_7'] : '';
 
-    $qrImgName = "$field_1"."$event_id".rand();
+    $fields = array(
+      'event_id' => isset($_POST['event_id']) ? $_POST['event_id'] : '',
+      'field_1' => isset($_POST['field_1']) ? $_POST['field_1'] : '',
+      'field_2' =>  isset($_POST['field_2']) ? $_POST['field_2'] : '',
+      'field_3' =>  isset($_POST['field_3']) ? $_POST['field_3'] : '',
+      'field_4' =>  isset($_POST['field_4']) ? $_POST['field_4'] : '',
+      'field_5' =>  isset($_POST['field_5']) ? $_POST['field_5'] : '',
+      'field_6' =>  isset($_POST['field_6']) ? $_POST['field_6'] : '',
+      'field_7' =>  isset($_POST['field_7']) ? $_POST['field_7'] : '',
+    );
+
+    $qrImgName = "$fields[1]"."$fields[0]".rand();
     $qrimage = $qrImgName.".png";
     $qrs = QRcode::png($qrimage,"plugins/userQr/$qrImgName.png","H","3","3");
     $workDir = $_SERVER['HTTP_HOST'];
     $qrlink = $workDir."/qrcode".$qrImgName.".png";
 
-    $fields = array("$field_1","$field_2","$field_3","$field_4","$field_5","$field_6","$field_7");
     ksort($fields);
 
     // Defining a callback function
@@ -30,25 +32,24 @@
     // Filtering the array
     $fields = array_filter($fields, "myFilter");
 
-    for ($i=0; $i < count($fields); $i++) { 
-      // code...
-      $field = $fields[$i];
-      if (isset($field)) {
-        // code...
-        $insQr = $register->insertQr($event_id,$fields,$qrimage,$qrlink);
+    foreach ($fields as $key => $value) {
+      $k[] = $key;
+      $v[] = "'".$value."'";
+    }
 
-        if($insQr == true) {
-            echo "<script>alert('Thank You. Success Create Your QR Code'); window.location='forms.php?success=$qrimage&field=$field_1';</script>";
-        }
-        else {
-          echo "<script>alert('cant create QR Code');
-          window.location='forms.php?event_id=$event_id';</script>";
-        }
-      }
-      else {
-        echo "<script>alert('Undefined field');
-          window.location='forms.php?event_id=$event_id';</script>";
-      }
+    $k = implode(",", $k);
+    $v = implode(",", $v);
+
+    $sql = "INSERT INTO registered ($k,qrImage,qrlink) VALUES ($v,'$qrimage','$qrlink')";
+    $result = mysqli_query($conn, $sql);
+
+    if($result) {
+      echo "<script>alert('Thank You. Success Create Your QR Code');
+      window.location='forms.php?success=$qrimage&field=$field_1';</script>";
+    }
+    else {
+      echo "<script>alert('cant create QR Code');
+      window.location='forms.php?event_id=$event_id';</script>";
     }
   }
 ?>
